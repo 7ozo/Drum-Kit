81778f1cd74bd2b114c3df2edb35576b1c874b40
Velpar <hforhoozoo@gmail.com>
2022-01-28 18:25:14 +0200

    fixed key-stuck bug in chrome

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const playSound = (e) => {
   //to work on capslock
   const keyData = e.key.toLowerCase();
   const audio = document.querySelector(`audio[data-key=${keyData}]`);
+  const keyDiv = document.querySelector(`div[data-key=${keyData}]`)
   //to prevent error from console
   if (!audio) return;
   // to spam the audio
   audio.currentTime = 0;
   audio.play();
   //adding playing style to the the key
-  document.querySelector(`div[data-key=${keyData}]`).classList.add("playing");
+  // the if statment to fix key-stuck bug
+  if(keyDiv.classList.contains('playing')){
+    keyDiv.classList.remove("playing");
+  }else {
+    keyDiv.classList.add("playing");
+  }
 };
 
 //add event to the key-down event
